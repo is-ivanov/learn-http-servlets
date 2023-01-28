12bec943c13248575a99d8025790aa38b1d9d995
ivanivanov <ISIvanov@lanit.ru>
2023-01-28 19:15:25 +0300

    After Lesson 9. Loop request/response

--- src/main/java/by/iivanov/socket/SocketRunner.java
+++ src/main/java/by/iivanov/socket/SocketRunner.java
@@ -5,6 +5,7 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.InetAddress;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class SocketRunner {
 
@@ -12,9 +13,13 @@ public class SocketRunner {
 		InetAddress googleAddress = InetAddress.getByName("localhost");
 		try (Socket socket = new Socket(googleAddress, 7777);
 			 DataOutputStream outputStream = new DataOutputStream(socket.getOutputStream());
-			 DataInputStream inputStream = new DataInputStream(socket.getInputStream())) {
-			outputStream.writeUTF("Hello, world!");
-			System.out.println("Response from server: " + inputStream.readUTF());
+			 DataInputStream inputStream = new DataInputStream(socket.getInputStream());
+			 Scanner scanner = new Scanner(System.in)) {
+			while (scanner.hasNextLine()) {
+				String request = scanner.nextLine();
+				outputStream.writeUTF(request);
+				System.out.println("Response from server: " + inputStream.readUTF());
+			}
 		}
 
 	}
