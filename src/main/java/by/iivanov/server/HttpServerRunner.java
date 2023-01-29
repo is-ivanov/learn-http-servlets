d561f0bd8d31d16f8b1ace27e24a5dd353e14026
ivanivanov <ISIvanov@lanit.ru>
2023-01-29 21:37:53 +0300

    After Lesson 18. Single threaded server

---
+++ src/main/java/by/iivanov/server/HttpServerRunner.java
@@ -0,0 +1,9 @@
+package by.iivanov.server;
+
+public class HttpServerRunner {
+
+	public static void main(String[] args) {
+		HttpServer httpServer = new HttpServer(9000);
+		httpServer.run();
+	}
+}
