1c2414b9e290e061e61f236db565e7c799866378
ivanivanov <ISIvanov@lanit.ru>
2023-02-02 22:08:23 +0300

    After Lesson 30. HTTP Body

--- src/by/iivanov/sevlet/FirstServlet.java
+++ src/by/iivanov/sevlet/FirstServlet.java
@@ -7,9 +7,11 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Map;
+import java.util.stream.Stream;
 
 @WebServlet("/first")
 public class FirstServlet  extends HttpServlet {
@@ -41,8 +43,10 @@ public class FirstServlet  extends HttpServlet {
 
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		Map<String, String[]> parameterMap = req.getParameterMap();
-		System.out.println(parameterMap.toString());
+		try (BufferedReader reader = req.getReader();
+			 Stream<String> lines = reader.lines()) {
+			lines.forEach(System.out::println);
+		}
 	}
 
 	@Override
