c8730c69c9ae55dd262508fc2e62133f2d9abbfe
ivanivanov <ISIvanov@lanit.ru>
2023-02-02 19:57:34 +0300

    After Lesson 28. Work with Headers

--- src/by/iivanov/sevlet/FirstServlet.java
+++ src/by/iivanov/sevlet/FirstServlet.java
@@ -20,9 +20,17 @@ public class FirstServlet  extends HttpServlet {
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		resp.setContentType("text/html");
+		var headerNames = req.getHeaderNames();
+		while (headerNames.hasMoreElements()) {
+			var header = headerNames.nextElement();
+			System.out.println(req.getHeader(header));
+		}
+
+		resp.setContentType("text/html; charset=UTF-8");
+//		resp.setCharacterEncoding(StandardCharsets.UTF_8.name());
+		resp.setHeader("token", "12345");
 		try (PrintWriter writer = resp.getWriter()) {
-			writer.write("<h1>Hello from First Servlet</h1>");
+			writer.write("<h1>Привет с первого сервлета!</h1>");
 		}
 
 	}
